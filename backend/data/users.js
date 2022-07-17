a83f3256dadbc7724915fd40b115fe98544f7b16
Salam Sheikh <dev.salamsheikh@gmail.com>
2022-07-17 23:24:43 +0600

    got some error on cart page with redux

---
+++ backend/data/users.js
@@ -0,0 +1,22 @@
+import bcrypt from "bcryptjs";
+
+const users = [
+  {
+    name: "Admin User",
+    email: "admin@example.com",
+    password: bcrypt.hashSync("123456", 10),
+    isAdmin: true,
+  },
+  {
+    name: "Salam Sheikh",
+    email: "salam@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+  {
+    name: "El",
+    email: "el@example.com",
+    password: bcrypt.hashSync("123456", 10),
+  },
+];
+
+export default users;
